b921d5b4c9f45ff6fa171a7e13e75c1cb876edb1
atrria <ada.lesna@gmail.com>
2020-10-20 01:32:11 +0200

    Lab2 exercise2b added

---
+++ Lab2/src/main/scala/exercise2b.scala
@@ -0,0 +1,37 @@
+/*
+Exercise 2b:
+We have a 2D array:
+val x = Array.ofDim[Int](4, 4);
+for (i <- 0 until 4; j <- 0 until 4) x(i)(j) = i + j
+We want to have a function: nicePrint taking that array as an argument and making nice printout like this:
+|0 1 2 3|
+|1 2 3 4|
+|2 3 4 5|
+|3 4 5 6|
+Each row has to be printed by an additional, inner function called niceRowPrint.
+ */
+
+object exercise2b
+{
+	def main(args: Array[String]): Unit =
+	{
+		val x = Array.ofDim[Int](4, 4)
+		for (i <- 0 until 4; j <- 0 until 4) x(i)(j) = i + j
+		
+		def nicePrint(arr: Array[Array[Int]]): Unit =
+		{
+			def niceRowPrint(row: Array[Int]): Unit = print(row.mkString("|", " ", "|\n"))
+			for (i <- arr.indices) niceRowPrint(arr(i))
+		}
+		
+		// niceRowPrint without .mkString
+		/*def niceRowPrint(row: Array[Int]): Unit =
+		{
+			/*print(s"|${row(0)}")
+			for (i <- 1 until row.length) print(s" ${row(i)}")
+			println("|")*/
+		}*/
+		
+		nicePrint(x)
+	}
+}
